32728b93d15b9391aaf3c5840e73af51cacd0629
fafoy <ihcaoe@gmail.com>
2018-05-20 21:21:46 +0800

    修華語顯示kah輕聲對齊

--- src/元件/一句.jsx
+++ src/元件/一句.jsx
@@ -12,7 +12,7 @@ export default class 一句 extends React.Component {
     if (分詞 && 漢字 && 臺羅閏號調) {
       let 分詞陣列 = 分詞.split(' ');
       let 漢字陣列 = 漢字.split(' ');
-      let 臺羅陣列 = 臺羅閏號調.split(' ');
+      let 臺羅陣列 = 臺羅閏號調.replace(/--/g, " --").trim().split(' ');
 
       let 詞 = 分詞陣列.map((分詞, i)=>(
         <一詞 key={i}
@@ -24,7 +24,11 @@ export default class 一句 extends React.Component {
         </div>
       );
     } else {
-      return null;
+      return (
+        <div className='tai5'>
+          <ruby>{分詞}</ruby>
+        </div>
+      );
     }
   }
 }
